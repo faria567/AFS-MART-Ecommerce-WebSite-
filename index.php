9d0d4c6c98e9ff16941367b364d7f5946e869654
Mossammat Faria <mossammatfaria1999@gmail.com>
2023-04-16 13:50:50 +0600

    commit

---
+++ index.php
@@ -0,0 +1,308 @@
+﻿<?php
+session_start();
+
+?>
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>AFS Mart</title>
+	 <link rel="shortcut icon" href="assets/img/AFS2.png" type="image/x-icon" />
+
+
+    <link href="assets/css/bootstrap.css" rel="stylesheet" />
+	<link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+	<link href="assets/css/flexslider.css" rel="stylesheet" />
+    <link href="assets/css/style.css" rel="stylesheet" />    
+ 
+</head>
+<body >
+   
+ <div class="navbar navbar-inverse navbar-fixed-top " id="menu">
+        <div class="container">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand" href="#"><img class="logo-custom" src="assets/img/AFS2.png" alt=""  /></a>
+            </div>
+            <div class="navbar-collapse collapse move-me">
+                <ul class="nav navbar-nav navbar-right">
+                    <li ><a href="index.html">Home</a></li>
+                     <li><a href="product.html">Shop</a></li>
+                   
+                     <li><a href="#brand-sec">About</a></li>
+					  <li><a href="signup.html">Contact</a></li>
+                     
+                </ul>
+            </div>
+           
+        </div>
+    </div>
+     
+       <div class="home-sec" id="home" >
+           <div class="overlay">
+ <div class="container">
+           <div class="row text-center " >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+                <div class="flexslider set-flexi" id="main-section" >
+                    <ul class="slides move-me">
+                        <!-- Slider 01 -->
+                        <li>
+                              <h3>High Quality Products</h3>
+                           <h1>WHAT ARE YOU WAITING FOR? SHOP NOW!</h1>
+                            <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                                SIGN IN
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                SIGN UP
+                            </a>
+							
+							<a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                ADMIN
+                            </a>
+                        </li>
+                        <!-- End Slider 01 -->
+                        
+                        <!-- Slider 02 -->
+                        <li>
+                            <h3>High Quality Products</h3>
+                           <h1>WE HAVE WHAT YOU NEED</h1>
+                             <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                               SIGN IN
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                SIGN UP
+                            </a>
+							
+							<a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                ADMIN
+                            </a>
+                        </li>
+                        <!-- End Slider 02 -->
+                        
+                        <!-- Slider 03 -->
+                        <li>
+                            <h3>High Quality  Products</h3>
+                           <h1>WE HAVE ALL BRANDS</h1>
+                             <a  href="#features-sec" class="btn btn-warning btn-lg" data-toggle="modal" data-target="#ln">
+                                SIGN IN
+                            </a>
+                             <a  href="#features-sec" class="btn btn-success btn-lg" data-toggle="modal" data-target="#su">
+                                SIGN UP
+                            </a>
+							<a  href="#features-sec" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#an">
+                                ADMIN
+                            </a>
+                        </li>
+                        <!-- End Slider 03 -->
+                    </ul>
+                </div>
+                   
+     
+              
+              
+            </div>
+                
+               </div>
+                </div>
+           </div>
+           
+       </div>
+
+
+
+
+
+
+
+
+
+       <!--HOME SECTION END-->   
+
+
+
+
+
+
+
+
+
+
+
+
+    <div  class="tag-line" >
+         <div class="container">
+           <div class="row  text-center" >
+           
+               <div class="col-lg-12  col-md-12 col-sm-12">
+               
+        <h2 data-scroll-reveal="enter from the bottom after 0.1s" ><i class="fa fa-circle-o-notch"></i> WELCOME TO THE AFS Mart <i class="fa fa-circle-o-notch"></i> </h2>
+                   </div>
+               </div>
+             </div>
+        
+    </div>
+    <!--HOME SECTION TAG LINE END-->   
+        
+
+         
+
+
+			
+     
+     <div class="modal fade" id="su" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 class="modal-title" id="myModalLabel">Registration Form</h4>
+              </div>
+              <div class="modal-body">
+            
+				
+				 <form role="form" method="post" action="register.php">
+                   <fieldset>
+					
+							<div class="form-group">
+                                <input class="form-control" placeholder="Firstname" name="ruser_firstname" type="text" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Lastname" name="ruser_lastname" type="text" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Address" name="ruser_address" type="text" required>
+							</div>
+							
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Email" name="ruser_email" type="email" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Password" name="ruser_password" type="password" required>
+							</div>
+				   
+					 </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-success  btn-block" name="register">Sign Up</button>
+				
+				 <button type="button" class="btn btn-md btn-danger btn-block" data-dismiss="modal">Cancel</button>
+				   </form>
+              </div>
+            </div>
+          </div>
+        </div>
+<!-- Script -->
+
+
+     <div class="modal fade" id="ln" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+            <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 style="color:white" class="modal-title" id="myModalLabel">Customer Login</h4>
+              </div>
+              <div class="modal-body">
+            
+				
+				 <form role="form" method="post" action="userlogin.php">
+                   <fieldset>
+					
+						
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Email" name="user_email" type="email" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Password" name="user_password" type="password" required>
+							</div>
+				   
+					 </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-success btn-block" name="user_login">Sign In</button>
+				
+				 <button type="button" class="btn btn-md btn-danger btn-block" data-dismiss="modal">Cancel</button>
+				   </form>
+				   
+              </div>
+            </div>
+          </div>
+        </div>
+		
+		<div class="modal fade" id="an" tabindex="-1" role="dialog" aria-labelledby="myMediulModalLabel">
+          <div class="modal-dialog modal-sm">
+           <div style="color:white;background-color:#008CBA" class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                <h4 style="color:white" class="modal-title" id="myModalLabel">Administrator Credentials</h4>
+              </div>
+              <div class="modal-body">
+            
+				
+				 <form role="form" method="post" action="adminlogin.php">
+                   <fieldset>
+					
+						
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Username" name="admin_username" type="text" required>
+							</div>
+							
+							<div class="form-group">
+                                <input class="form-control" placeholder="Password" name="admin_password" type="password" required>
+							</div>
+				   
+					 </fieldset>
+                  
+            
+              </div>
+              <div class="modal-footer">
+               
+                <button class="btn btn-md btn-success btn-block" name="admin_login">Login</button>
+				
+				 <button type="button" class="btn btn-md btn-danger btn-block" data-dismiss="modal">Cancel</button>
+				   </form>
+              </div>
+            </div>
+          </div>
+        </div>
+		 <br />
+			 <br />
+			 <br />
+<!-- Script -->
+     <!-- CONTACT SECTION END-->
+    <div id="footer">
+      <a style="color: #fff" target="_blank">Design by : AFS Mart</a>
+    </div>
+     <!-- FOOTER SECTION END-->
+   
+    <!--  Jquery Core Script -->
+    <script src="assets/js/jquery-1.10.2.js"></script>
+    <!--  Core Bootstrap Script -->
+    <script src="assets/js/bootstrap.js"></script>
+    <!--  Flexslider Scripts --> 
+         <script src="assets/js/jquery.flexslider.js"></script>
+     <!--  Scrolling Reveal Script -->
+    <script src="assets/js/scrollReveal.js"></script>
+    <!--  Scroll Scripts --> 
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <!--  Custom Scripts --> 
+         <script src="assets/js/custom.js"></script>
+</body>
+</html>
